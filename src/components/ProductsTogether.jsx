49b3c8a54c01dbc83d6f427a5c8c4fec818aba86
Surya Tejess <ksuryatejess@gmail.com>
2026-01-15 14:21:46 +0530

    fetch products from backend instead of hardcoding

--- src/components/ProductsTogether.jsx
+++ src/components/ProductsTogether.jsx
@@ -1,23 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductCard from './ProductCard';
+import.meta.env.VITE_API_BASE_URL
+
 
 const ProductsTogether = () => {
+
+  console.log("bakendUrl is " + import.meta.env.VITE_API_BASE_URL); 
+  const backendUrl = import.meta.env.VITE_API_BASE_URL; 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        const response = await fetch(`${backendUrl}/product/`);
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch products");
+        }
+
+        const data = await response.json();
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchProducts();
+  }, [backendUrl]);
+
+  if (loading) return <p className="p-8">Loading products...</p>;
+  if (error) return <p className="p-8 text-red-500">{error}</p>;
 
     const image_ = "https://images.unsplash.com/photo-1555041469-a586c61ea9bc?w=800&auto=format&fit=crop&q=80"; 
     const name_ = "Minimal ceramic vase"; 
     const price_ = 89; 
     
-    
   return (
     <>
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 p-8'>
-            <ProductCard image={image_} name={name_} price={price_} />
-            <ProductCard image={image_} name={name_} price={price_} />
-            <ProductCard image={image_} name={name_} price={price_} />
-            <ProductCard image={image_} name={name_} price={price_} />
-            <ProductCard image={image_} name={name_} price={price_} />
-            <ProductCard image={image_} name={name_} price={price_} />
-            
+            {products.map(product => (
+                <ProductCard
+                  key={product.id}
+                  image={product.image}
+                  name={product.name}
+                  price={product.price}
+                />
+               )
+             )
+            }
         </div>
     </>
   )
