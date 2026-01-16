54f9a12b086c3470f2d48b675d3f63fea1626110
Surya Tejess <ksuryatejess@gmail.com>
2026-01-16 16:39:08 +0530

    bug fix : when pressing the + ( add to cart ) button on the product page, it is getting redirected to thta product page after getting added to the cart. now, the product will be added to the cart but wont be redirected to that particular product page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -24,28 +24,30 @@ const ProductCard = (props) => {
     return (
         <>
             <div className="flex flex-col mx-4">
-                {/* image */}
-                <a
-                    href={`/product/${props.id}`}
-                    className="border-2 p-0 aspect-square relative mb-4 overflow-hidden rounded-lg"
-                >
-                    <img
-                        src={props.image}
-                        className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
-                        alt=""
-                    />
+                {/* CARD */}
+                <div className="border-2 p-0 aspect-square relative mb-4 overflow-hidden rounded-lg">
+                    {/* IMAGE LINK */}
+                    <a
+                        href={`/product/${props.id}`}
+                        className="block w-full h-full"
+                    >
+                        <img
+                            src={props.image}
+                            className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
+                            alt=""
+                        />
+                    </a>
 
-                    {/* TODO : when button onClick add to cart */}
-                    {/* button to add to cart */}
+                    {/* ADD TO CART BUTTON */}
                     <button
                         onClick={addToCart}
-                        className="absolute bottom-3 right-3 rounded-md bg-black text-white flex items-center justify-center transition-opacity px-4 py-3 cursor-pointer hover:bg-amber-600 hover:scale-110"
+                        className="absolute bottom-3 right-3 rounded-md bg-black text-white flex items-center justify-center transition-opacity px-4 py-3 cursor-pointer hover:bg-amber-600 hover:scale-110 z-10"
                     >
                         +
                     </button>
-                </a>
+                </div>
 
-                {/* desc : name, price */}
+                {/* NAME & PRICE */}
                 <div>
                     <h3 className="text-sm font-light">{props.name}</h3>
                     <p className="text-sm font-medium">{props.price}</p>
